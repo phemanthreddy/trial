916924a280471a8e99698f6e7caa70b6396cf5fc
phemanthreddy <98023837+phemanthreddy@users.noreply.github.com>
2022-02-27 08:27:56 +0530

    Add files via upload

---
+++ SpringCore/Question3/BankAccount.java
@@ -0,0 +1,48 @@
+package Question3;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BankAccount {
+
+	long accountId;
+	String accountHolderName;
+	String accountType;
+	double accountBalance;
+
+	public long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(long accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+}
+
+
+
+
---
+++ SpringCore/Question3/BankAccountRepository.java
@@ -0,0 +1,7 @@
+package Question3;
+public interface BankAccountRepository {
+
+	public double getBalance(long accountId);
+
+	public double updateBalance(long accountId, double newBalance, String type);
+}
