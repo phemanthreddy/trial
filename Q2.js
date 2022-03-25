d7f141585b0016e9b4e18fad7b0e4e2cbee4cdf0
phemanthreddy <98023837+phemanthreddy@users.noreply.github.com>
2022-03-25 14:18:40 +0530

    Add files via upload

---
+++ Q2.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+export class Q2 extends Component {
+    render() {
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Job</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Charlie</td>
+                            <td>Janitor</td>
+                        </tr>
+                        <tr>
+                            <td>Mac</td>
+                            <td>Bouncer</td>
+                        </tr>
+                        <tr>
+                            <td>Dee</td>
+                            <td>Aspiring Actress</td>
+                        </tr>
+                        <tr>
+                            <td>Dennis</td>
+                            <td>Bartender</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default Q2
