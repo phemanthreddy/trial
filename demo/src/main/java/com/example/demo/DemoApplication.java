bc41ef2f79bcc395734167d49b038436adeba795
hemanth <hemanthreddyp11@gmail.com>
2022-04-20 18:56:16 +0530

    first commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("kn");
 		System.out.println("ln");
-		System.out.println("lkbrgtyyrn");
+		System.out.println("ltyyrn");
 	}
 
 }
